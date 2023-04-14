e12d661aad5e99d9cf5f6a44b4106c5adff9a1a9
ForhadRoben <i.rforhad@gmail.com>
2023-04-14 15:21:02 +0600

    error page created

--- src/components/ErrorPage.jsx
+++ src/components/ErrorPage.jsx
@@ -1,11 +1,41 @@
-import React from 'react';
+import React from 'react'
+import { Link, useRouteError } from 'react-router-dom'
+
 
 const ErrorPage = () => {
+    const error = useRouteError()
     return (
-        <div>
-            <p>this is error page</p>
-        </div>
-    );
-};
+        <section className='flex items-center h-screen p-16 bg-secondary text-gray-700'>
+            <div className='container flex flex-col items-center justify-center px-5 mx-auto my-8'>
+                <div className='max-w-md text-center'>
+                    <h2 className='mb-8 font-extrabold text-9xl text-red-600'>
+                        {
+                            error &&
+                            <div>
+                                <h1 className='text-xl font-bold'>{error.status}</h1>
+                                <p className='text-red-500 text-3xl'>{error.statusText || error.message}</p>
+                            </div>
+                        }
+                    </h2>
+                    <p className='text-2xl font-semibold text-blue-500 md:text-3xl mb-8'>
+                        OOPS! PAGE NOT FOUND
+                    </p>
+                    <Link
+                        to='/'
+                        className='px-8 py-3 font-bold rounded bg-blue-300 text-red-500'
+                    >
+                        Go Home
+                    </Link>
+                    <Link
+                        to='/'
+                        className='px-8 py-3 mx-8 font-bold rounded bg-blue-300 text-red-500'
+                    >
+                        Contact Us
+                    </Link>
+                </div>
+            </div>
+        </section>
+    )
+}
 
-export default ErrorPage;
+export default ErrorPage
--- src/main.jsx
+++ src/main.jsx
@@ -3,12 +3,21 @@ import ReactDOM from 'react-dom/client'
 import App from './App'
 import './index.css'
 import { RouterProvider, createBrowserRouter } from 'react-router-dom'
+import ErrorPage from './components/ErrorPage'
+import Home from './components/Home'
 
 const router = createBrowserRouter([
 
   {
     path: "/",
     element: <App></App>,
+    errorElement: <ErrorPage></ErrorPage>,
+    children: [
+      {
+        path: "/",
+        element: <Home></Home>
+      }
+    ]
 
   }
 ])
