0ddb3942e3f6404be4ded73090753e4bdab873f1
ForhadRoben <i.rforhad@gmail.com>
2023-04-14 18:01:30 +0600

    category list section commited

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,30 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import Header from './Header';
+import SingleCategory from './SingleCategory';
+
+
 
 const Home = () => {
+    const [categories, setCategories] = useState([])
+    useEffect(() => {
+        fetch("/category.json")
+            .then(res => res.json())
+            .then(data => setCategories(data))
+    }, [])
+
     return (
         <div>
             <Header></Header>
+            <h1 className='text-center text-2xl font-semibold'>Job Category List</h1>
+            <p className='text-center'>Explore thousands of job opportunities with all the information you need. Its your future</p>
+            <div className='grid lg:grid-cols-4 gap-4 m-4'>
+
+                {
+                    categories.map(category => <SingleCategory
+                        key={category.id}
+                        category={category}></SingleCategory>)
+                }
+            </div>
             <p>this is home</p>
         </div>
     );
---
+++ src/components/SingleCategory.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+const SingleCategory = ({ category }) => {
+    console.log(category);
+    const { id, category_logo, category_name, available_job } = category;
+    // const { }
+    return (
+        <div className="card w-full bg-secondary">
+            <figure className="pr-24 pt-10">
+                <img src={category_logo} alt="job logo" className="rounded-xl bg-gray-200 p-4" />
+            </figure>
+            <div className="card-body items-center text-center">
+                <h2 className="card-title">{category_name}</h2>
+                <p>{available_job} jobs available</p>
+            </div>
+        </div>
+    );
+};
+
+export default SingleCategory;
